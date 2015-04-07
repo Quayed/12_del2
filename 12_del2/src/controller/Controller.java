bb5b46900644456209320e39d6d527fbbc0daafe
al <a@saloweb.dk>
2015-04-07 14:21:48 +0200

    changes

--- 12_del2/src/controller/Controller.java
+++ 12_del2/src/controller/Controller.java
@@ -3,67 +3,50 @@ package controller;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
-import entity.IData;
 import logic.ILogic;
-import boundary.IUI;
+import entity.IData;
 
 public class Controller {
 
 	private static final String DEFAULT_HOST = "localhost";
-	private static final int DEFAULT_PORT = 4567;
+	private static final int DEFAULT_PORT = 8000;
 
-	@SuppressWarnings("unused")
-	private IUI ui;
-	@SuppressWarnings("unused")
 	private ILogic logic;
-	@SuppressWarnings("unused")
 	private IData data;
 
-	private Thread readController;
-	private Thread writeController;
-
-	public Controller(IUI ui, ILogic logic, IData data) {
-		this.ui = ui;
+	public Controller(ILogic logic, IData data) {
 		this.logic = logic;
 		this.data = data;
 
 		// asks for connecting to server on startup
-		connect(ui, logic);
-
-		// after connecting, start the Read and Write controller threads
-		readController = new ReadController(ui, data);
-		readController.start();
-
-		writeController = new WriteController(ui, logic);
-		writeController.start();
+		connect(logic);
+		
+		start();
+	}
+	
+	private void start(){
+		logic.readMessage("Indtast nr.");
+		while(!data.hasData())
+			data.pullData();
+		
+		logic.readMessage("Indtast raavare nr.");
+		while(!data.hasData())
+			data.pullData();
 	}
 
 	// once called, this method will ask the user to connect by specifying a relevant address until a connection has been made
-	static void connect(IUI ui, ILogic logic) {
-		String[] hostAndPort;
-		String host;
-		int port;
+	static void connect(ILogic logic) {
 		boolean connectionError;
 
 		do {
 			connectionError = false;
-			hostAndPort = ui.getHostAndPort(DEFAULT_HOST, DEFAULT_PORT);
-			if (hostAndPort[0].equals("exit"))
-				System.exit(0);
-			host = hostAndPort[0];
-
 			try {
-				port = Integer.parseInt(hostAndPort[1]);
-				logic.connect(host, port);
-			} catch (NumberFormatException e) {
-				connectionError = true;
-				ui.message("Unknown port");
+				logic.connect(DEFAULT_HOST, DEFAULT_PORT);
 			} catch (UnknownHostException e) {
 				connectionError = true;
-				ui.message("Unknown host");
+				
 			} catch (IOException e) {
-				connectionError = true;
-				ui.message("Could't connect");
+				connectionError = true;;
 			}
 		} while (connectionError);
 	}
