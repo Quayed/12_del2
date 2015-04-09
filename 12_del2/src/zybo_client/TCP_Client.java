f1e57e4aeeeb095688ee29b1f2547d5d6e31a603
hippomormor <hippomormor@10.16.134.168>
2015-04-09 13:12:32 +0200

    TCP_Client added again

--- 12_del2/src/zybo_client/TCP_Client.java
+++ 12_del2/src/zybo_client/TCP_Client.java
@@ -2,18 +2,24 @@ package zybo_client;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class TCP_Client
 {
-
-    public static void main(String argv[]) throws Exception
+    private String sentence;
+    private String modifiedSentence;
+    
+    public void TCP_Client() throws IOException
     {
-        String sentence;
-        String modifiedSentence;
+
+    }
+    
+    public void send() throws IOException
+    {
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        Socket clientSocket = new Socket("192.168.0.39", 8001);
+        Socket clientSocket = new Socket("2.108.207.65", 8001);
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         sentence = inFromUser.readLine();
