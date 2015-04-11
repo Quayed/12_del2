a046f8d0cc98c251ba3bb1b00547b18dcb6289de
Hippomormor <Hippomormor@HIPPOMORMOR-PC>
2015-04-11 18:45:59 +0200

    Added timestamps to TCP_Client

--- 12_del2/src/zybo_client/TCP_Client.java
+++ 12_del2/src/zybo_client/TCP_Client.java
@@ -1,22 +1,25 @@
 package zybo_client;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import shared.SocketHandler;
 
 public class TCP_Client {
 
 	private String modifiedSentence;
-	private SocketHandler socketHandler;
+	private final SocketHandler socketHandler;
 
 	public TCP_Client() throws IOException {
 		socketHandler = new SocketHandler("localhost", 8001);
 	}
 
 	public void send(String output) throws IOException {
+                SimpleDateFormat sdataSocket = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		socketHandler.println(output);
 		modifiedSentence = socketHandler.readLine();
-		System.out.println("FROM SERVER: " + modifiedSentence);
+		System.out.println("\n" + sdataSocket.format(new Date()) + " - FROM SERVER: " + modifiedSentence);
 	
 	}
 }
