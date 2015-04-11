b442bc2d5b96287a04612828c804d093b6dcea20
Hippomormor <Hippomormor@HIPPOMORMOR-PC>
2015-04-11 18:27:43 +0200

    Made ip in FTP_Client constant

--- 12_del2/src/zybo_client/Zybo_Main.java
+++ 12_del2/src/zybo_client/Zybo_Main.java
@@ -30,102 +30,102 @@ public class Zybo_Main
             int type = key.nextInt();
             if (type == 1)
             {
-                    System.out.println("\nEnter username: ");
-                    key.nextLine();
-                    user = key.nextLine();
-                    if (!user.equals(""))
+                System.out.println("\nEnter username: ");
+                key.nextLine();
+                user = key.nextLine();
+                if (!user.equals(""))
+                {
+                    System.out.println("\nEnter password: ");
+                    pass = key.nextLine();
+                    if (!pass.equals(""))
                     {
-                        System.out.println("\nEnter password: ");
-                        pass = key.nextLine();
-                        if (!pass.equals(""))
+                        try
+                        {
+                            System.out.println("\n" + sdataSocket.format(new Date()) + " - Connecting to server...");
+
+                            if (FTP.connect(user, pass))
+                            {
+                                connected = true;
+                            }
+                            else
+                            {
+                                System.out.println("\nCredentials denied!\n");
+                            }
+                        }
+                        catch (ConnectException e)
+                        {
+                            //e.printStackTrace();
+                            System.out.println("\n" + sdataSocket.format(new Date()) + " - Connection timed out.");
+                        }
+                        catch (UnknownHostException e)
+                        {
+                            //e.printStackTrace();
+                            System.out.println("\n" + sdataSocket.format(new Date()) + " - Unknown host.");
+                        }
+                        catch (SocketException e)
+                        {
+                            //e.printStackTrace();
+                            System.out.println("\n" + sdataSocket.format(new Date()) + " - Network is unreachable.");
+                        }
+
+                    }
+                }
+                while (connected)
+                {
+                    System.out.println("Type '0' to return to main menu\n\nType '1' to list files:\n\nType '2' to retrieve file:\n\nType '3' to delete file:");
+                    type = key.nextInt();
+                    if (type == 1)
+                    {
+                        String answer = FTP.getData("LIST");
+                        if (answer.length() > 1)
+                        {
+                            System.out.println("\nFiles: " + answer);
+                        }
+                        else
+                        {
+                            System.out.println("\nNo files on Zybo!");
+                        }
+                    }
+                    else if (type == 2)
+                    {
+                        System.out.println("\nEnter filename:\n");
+                        key.nextLine();
+                        String name = key.nextLine();
+                        String answer = FTP.getData("RETR " + name);
+                        System.out.println(answer);
+                        if (!answer.equals("\nFile not found"))
                         {
                             try
                             {
-                                System.out.println("\n" + sdataSocket.format(new Date()) + " - Connecting to server...");
-
-                                if (FTP.connect(user, pass))
-                                {
-                                    connected = true;
-                                }
-                                else
-                                {
-                                    System.out.println("\nCredentials denied!\n");
-                                }
+                                FileWriter file = new FileWriter("ftp-files/" + name.split("/")[name.split("/").length - 1]);
+                                PrintWriter out = new PrintWriter(file);
+                                out.write(answer);
+                                out.close();
+                                System.out.println("\n" + sdataSocket.format(new Date()) + " - <" + name + "> has been downloaded succesfully!");
                             }
-                            catch (ConnectException e)
+                            catch (FileNotFoundException e)
                             {
                                 //e.printStackTrace();
-                                System.out.println("\n" + sdataSocket.format(new Date()) + " - Connection timed out.");
+                                System.out.println("\n" + sdataSocket.format(new Date()) + " - File not writable on local disk!");
                             }
-                            catch (UnknownHostException e)
-                            {
-                                //e.printStackTrace();
-                                System.out.println("\n" + sdataSocket.format(new Date()) + " - Unknown host.");
-                            }
-                            catch (SocketException e)
-                            {
-                                //e.printStackTrace();
-                                System.out.println("\n" + sdataSocket.format(new Date()) + " - Network is unreachable.");
-                            }
-
+                        }
+                        else
+                        {
+                            System.out.println("\n" + sdataSocket.format(new Date()) + " - 550 " + answer + ".");
                         }
                     }
-                    while (connected)
+                    else if (type == 3)
                     {
-                        System.out.println("Type '0' to return to main menu\n\nType '1' to list files:\n\nType '2' to retrieve file:\n\nType '3' to delete file:");
-                        type = key.nextInt();
-                        if (type == 1)
-                        {
-                            String answer = FTP.getData("LIST");
-                            if (answer.length() > 1)
-                            {
-                                System.out.println("\nFiles: " + answer);
-                            }
-                            else
-                            {
-                                System.out.println("\nNo files on Zybo!");
-                            }
-                        }
-                        else if (type == 2)
-                        {
-                            System.out.println("\nEnter filename:\n");
-                            key.nextLine();
-                            String name = key.nextLine();
-                            String answer = FTP.getData("RETR " + name);
-                            System.out.println(answer);
-                            if (!answer.equals("\nFile not found"))
-                            {
-                                try
-                                {
-                                    FileWriter file = new FileWriter("ftp-files/" + name.split("/")[name.split("/").length - 1]);
-                                    PrintWriter out = new PrintWriter(file);
-                                    out.write(answer);
-                                    out.close();
-                                    System.out.println("\n" + sdataSocket.format(new Date()) + " - <" + name + "> has been downloaded succesfully!");
-                                }
-                                catch (FileNotFoundException e)
-                                {
-                                    //e.printStackTrace();
-                                    System.out.println("\n" + sdataSocket.format(new Date()) + " - File not writable on local disk!");
-                                }
-                            }
-                            else
-                            {
-                                System.out.println("\n" + sdataSocket.format(new Date()) + " - 550 " + answer + ".");
-                            }
-                        }
-                        else if (type == 3)
-                        {
-                            System.out.println("\nEnter filename:\n");
-                            key.nextLine();
-                            String name = key.nextLine();
-                            System.out.println("\n" + sdataSocket.format(new Date()) + " - " + FTP.ftpHandler.send("DELE " + name));
-                        }
-                        else if (type == 0)
-                        {
-                            break;
-                        }
-                    }               
+                        System.out.println("\nEnter filename:\n");
+                        key.nextLine();
+                        String name = key.nextLine();
+                        System.out.println("\n" + sdataSocket.format(new Date()) + " - " + FTP.ftpHandler.send("DELE " + name));
+                    }
+                    else if (type == 0)
+                    {
+                        break;
+                    }
+                }
             }
             else if (type == 2)
             {
