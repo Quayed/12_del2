0c52f5e1df19f1cba59d7a7394814aa39e312f27
Hippomormor <Hippomormor@HIPPOMORMOR-PC>
2015-04-14 01:20:55 +0200

    sleep

--- 12_del2/src/zybo_client/FTPHandler.java
+++ 12_del2/src/zybo_client/FTPHandler.java
@@ -29,7 +29,7 @@ public class FTPHandler extends SocketHandler {
                     }
                     catch (InterruptedException e)
                     {
-                        e.printStackTrace();;
+                        
                     }
 		}
 	}
